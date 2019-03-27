0ecbbefb41306557257e13bb40cf601c89a22c0c
Serhiy Ivaneyko <ivaneyko.serhiy@gmail.com>
2019-03-27 17:50:16 +0200

    Deploy

---
+++ precache-manifest.50253bda5b9aa2958ffe19c6c44b2974.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff8d9f8adb0d09f11d4816a152672f53",
+    "url": "/static/media/fa-solid-900.ff8d9f8a.ttf"
+  },
+  {
+    "revision": "a7140145ebaaf5fb14e40430af5d25c4",
+    "url": "/static/media/fa-solid-900.a7140145.woff"
+  },
+  {
+    "revision": "49279363201ed19a840796e05a74a91b",
+    "url": "/static/media/fa-solid-900.49279363.svg"
+  },
+  {
+    "revision": "46e7cec623d8bd790d9fdbc8de2d3ee7",
+    "url": "/static/media/fa-solid-900.46e7cec6.eot"
+  },
+  {
+    "revision": "418dad87601f9c8abd0e5798c0dc1feb",
+    "url": "/static/media/fa-solid-900.418dad87.woff2"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "acfc8d5db8a4c0cce4fa",
+    "url": "/static/js/main.5e3762a7.chunk.js"
+  },
+  {
+    "revision": "246d084e7bd7a4f3b020",
+    "url": "/static/js/2.548997a6.chunk.js"
+  },
+  {
+    "revision": "acfc8d5db8a4c0cce4fa",
+    "url": "/static/css/main.3b6c9832.chunk.css"
+  },
+  {
+    "revision": "36e262aec486c9d890e239a5fe1dc8d5",
+    "url": "/index.html"
+  }
+];
