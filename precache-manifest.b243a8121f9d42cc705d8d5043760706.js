21a534f7d73c514894d0ed829963f4629974a83b
Serhiy Ivaneyko <ivaneyko.serhiy@gmail.com>
2019-12-04 23:15:23 +0200

    Updates

---
+++ precache-manifest.b243a8121f9d42cc705d8d5043760706.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f770d84a3e66ca852f91107c5957bbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc7ccd85e4524f499ccd",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "2e3a6a1b7b03c07dffb1",
+    "url": "/static/js/2.0b4dcf63.chunk.js"
+  },
+  {
+    "revision": "bc7ccd85e4524f499ccd",
+    "url": "/static/js/main.764fde0e.chunk.js"
+  },
+  {
+    "revision": "ca73b8fa923699440ec0",
+    "url": "/static/js/runtime-main.16da7107.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
