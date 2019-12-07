434129149ffa411c1b3f8e37b65e5dee8f8fca13
Serhiy Ivaneyko <ivaneyko.serhiy@gmail.com>
2019-12-07 11:20:06 +0200

    Updates

---
+++ precache-manifest.c2783cdc845012a9a9106878d65f83b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9299b3784417923920c24fa34f98de0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7107b5ec7cddbb290d60",
+    "url": "/static/css/main.ef497a45.chunk.css"
+  },
+  {
+    "revision": "06b453f3b634551dc80f",
+    "url": "/static/js/2.4d1efc7a.chunk.js"
+  },
+  {
+    "revision": "7107b5ec7cddbb290d60",
+    "url": "/static/js/main.d0660eeb.chunk.js"
+  },
+  {
+    "revision": "ca73b8fa923699440ec0",
+    "url": "/static/js/runtime-main.16da7107.js"
+  },
+  {
+    "revision": "496e277928fecd6f39192cea99439bd6",
+    "url": "/static/media/brand.496e2779.jpg"
+  }
+]);
